f6210180fe3b673ed73c5f232c3b4805deeac48c
vishal27shetty <vishal27shetty@gmail.com>
2024-06-09 11:41:04 +0530

    GCD part 4

---
+++ GCD_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	    int n1,n2,gcd,i;
+	        cout << "Enter two integer numbers:" << endl;
+		    cin >> n1 >> n2;
+		        for(i=1;i<=n1 && i<=n2; ++i) {
+				        if(n1%i==0 && n2%i==0)
+						            gcd=i;
+					    }
+			    cout << "The gcd is :" << gcd << endl;
+			        return 0;
+}
