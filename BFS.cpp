f6c5387b33a76e13fcf34223ac64f238c36907d5
Vishal Shetty <112746935+vishal27shetty@users.noreply.github.com>
2024-06-10 10:30:55 +0530

    BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,54 +1,50 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 
 using namespace std;
 
-vector<vector<int>> a(20, vector<int>(20));
-vector<int> visited(20, 0);
-int n;
+int a[20][20], q[20], visited[20], n;
+int front = -1, rear = -1;
 
 void bfs(int v) {
-    queue<int> q;
-    q.push(v);
-    visited[v] = 1;
-    
-    cout << v << " ";
-
-    while (!q.empty()) {
-        int front = q.front();
-        q.pop();
-
-        for (int i = 0; i < n; i++) {
-            if (a[front][i] != 0 && visited[i] == 0) {
-                q.push(i);
-                visited[i] = 1;
-                cout << i << " ";
-            }
+    for (int i = 0; i < n; i++) { // check all the vertices in the graph
+        if (a[v][i] != 0 && visited[i] == 0) { // adjacent to v and not visited
+            rear = rear + 1;
+            q[rear] = i; // insert them into queue
+            visited[i] = 1; // mark the vertex visited
+            cout << i << " ";
         }
+    }
+    front = front + 1; // remove the vertex at front of the queue
+    if (front <= rear) { // as long as there are elements in the queue
+        bfs(q[front]); // perform bfs again on the vertex at front of the queue
     }
 }
 
 int main() {
-    int v;
-    cout << "Enter the number of vertices: ";
+    int v, i, j;
+    cout << "\n Enter the number of vertices: ";
     cin >> n;
-
-    cout << "Enter graph data in matrix form:\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (i = 0; i < n; i++) { // mark all the vertices as not visited
+        visited[i] = 0;
+    }
+    cout << "\n Enter graph data in matrix form:\n";
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             cin >> a[i][j];
         }
     }
-
-    cout << "Enter the starting vertex: ";
+    cout << "\n Enter the starting vertex: ";
     cin >> v;
-
-    cout << "BFS traversal is:\n";
+    front = rear = 0;
+    q[rear] = v;
+    cout << "\n BFS traversal is:\n";
+    visited[v] = 1; // mark the starting vertex as visited
+    cout << v << " ";
     bfs(v);
 
     bool allVisited = true;
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         if (visited[i] == 0) {
             allVisited = false;
             break;
@@ -56,10 +52,8 @@ int main() {
     }
 
     if (!allVisited) {
-        cout << "\nBFS is not possible for all nodes.";
+        cout << "\n BFS is not possible for all nodes.";
     }
 
     return 0;
 }
-
-
