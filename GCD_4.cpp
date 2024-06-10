7f1290bbb97789933f8180f514d332b9ac93312c
Vishal Shetty <112746935+vishal27shetty@users.noreply.github.com>
2024-06-10 09:33:56 +0530

    GCD_4.cpp

--- GCD_4.cpp
+++ GCD_4.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-	    int n1,n2,gcd,i;
-	        cout << "Enter two integer numbers:" << endl;
-		    cin >> n1 >> n2;
-		        for(i=1;i<=n1 && i<=n2; ++i) {
-				        if(n1%i==0 && n2%i==0)
-						            gcd=i;
-					    }
-			    cout << "The gcd is :" << gcd << endl;
-			        return 0;
+  int n1, n2, gcd, i;
+  cout << "Enter two integer numbers:" << endl;
+  cin >> n1 >> n2;
+  for (i = 1; i <= n1 && i <= n2; ++i) {
+    if (n1 % i == 0 && n2 % i == 0)
+      gcd = i;
+  }
+  cout << "The gcd is :" << gcd << endl;
+  return 0;
 }
