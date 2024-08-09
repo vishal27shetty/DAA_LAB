ca6801bac85f229f6a4f50572279ce5e589b41c0
Vishal Shetty <112746935+vishal27shetty@users.noreply.github.com>
2024-08-09 18:00:15 +0530

    Update prim.cpp

--- prim.cpp
+++ prim.cpp
@@ -12,6 +12,7 @@ void prim(vector<vector<P>>& graph, int start) {
     int n = graph.size();
     vector<int> key(n, INT_MAX);
     vector<bool> inMST(n, false);
+    vector<int> parent(n, -1);  // To store the parent of each vertex in the MST
     priority_queue<P, vector<P>, greater<P>> pq;
 
     key[start] = 0;
@@ -30,35 +31,49 @@ void prim(vector<vector<P>>& graph, int start) {
             if (!inMST[v] && key[v] > wt) {
                 key[v] = wt;
                 pq.push({wt, v});
+                parent[v] = u; // Store the parent of v
             }
         }
     }
 
     for (int i = 0; i < n; ++i) {
         if (i != start) {
-            cout << "Edge: " << i << " - Cost: " << key[i] << endl;
+            cout << "Edge: " << parent[i] << " - " << i << " - Cost: " << key[i] << endl;
         }
     }
 }
 
 int main() {
-    int n = 5; // Number of vertices
+    int n = 6; // Number of vertices
     vector<vector<P>> graph(n);
 
-    graph[0].push_back({2, 1});
-    graph[0].push_back({3, 3});
-    graph[1].push_back({2, 0});
-    graph[1].push_back({3, 2});
-    graph[1].push_back({1, 3});
-    graph[2].push_back({3, 1});
-    graph[2].push_back({5, 4});
-    graph[3].push_back({3, 0});
-    graph[3].push_back({1, 1});
-    graph[3].push_back({2, 4});
-    graph[4].push_back({5, 2});
-    graph[4].push_back({2, 3});
+     graph[0].push_back({6, 3}); // A-D
+    graph[0].push_back({5, 1}); // A-B
+    graph[0].push_back({4, 4}); // A-E
+    graph[0].push_back({2, 2}); // A-C
 
-    prim(graph, 0);
+    graph[1].push_back({5, 0}); // B-A
+    graph[1].push_back({2, 2}); // B-C
+
+    graph[2].push_back({2, 1}); // C-B
+    graph[2].push_back({3, 4}); // C-E
+    graph[2].push_back({2, 0}); // C-A
+
+    graph[3].push_back({6, 0}); // D-A
+    graph[3].push_back({7, 5}); // D-F
+    graph[3].push_back({3, 4}); // D-E
+
+    graph[4].push_back({4, 0}); // E-A
+    graph[4].push_back({3, 3}); // E-D
+    graph[4].push_back({8, 5}); // E-F
+    graph[4].push_back({3, 2}); // E-C
+
+    graph[5].push_back({7, 3}); // F-D
+    graph[5].push_back({8, 4}); // F-E
+
+    // Start from vertex F, which is vertex 5 in 0-indexed array
+    prim(graph, 5);
 
     return 0;
 }
+
